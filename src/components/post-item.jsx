68426725063e90165eeae3fbeeefc6e689925b90
YangJ0605 <1442122744@qq.com>
2020-08-08 11:46:08 +0800

    add month map

--- src/components/post-item.jsx
+++ src/components/post-item.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import day from 'dayjs'
 
 export const PostWrapper = styled.div`
 max-width: 1400px;
@@ -12,21 +13,34 @@ flex-wrap: wrap;
   width: 25%;
   min-width: 25%;
   max-width: 25%;
-  height: 400px;
+  max-height: 400px;
   padding: 10px 10px;
   overflow: hidden;
   img {
-    height: 100%;
+    height: 90%;
     width: 100%;
     object-fit: cover;
+  }
+  footer {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
   }
 }
 `
 
 const PostItem = ({post}) =>  {
+  const month = day(post.date).month()
+  const year = day(post.date).year()
   return  (
     <div className='item'>
       <img src={post.image}/>
+      <footer>
+        <div className='ig_hanle'>{post.ig_handle}</div>
+        <div className='date'>{month} {year}</div>
+      </footer>
     </div>
    )
 }
