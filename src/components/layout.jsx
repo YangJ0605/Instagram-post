ef2e72603eb321b0b7e64d55052e853092f1cfa9
YangJ0605 <1442122744@qq.com>
2020-08-08 14:19:31 +0800

    add lazy-load

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 
 const Layout = styled.div`
-  max-width: 1400px;
+  max-width: 1200px;
   margin: 0 auto;
 `
 
--- src/components/post-item.jsx
+++ src/components/post-item.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import styled from 'styled-components'
 import day from 'dayjs'
+import LazyLoad from 'react-lazyload'
+import {monthObj} from '../utils/month'
+import Loading from '../components/Loading'
+// import logo from '../utils/logo512.png'
 
 export const PostWrapper = styled.div`
-max-width: 1400px;
+max-width: 1200px;
 display: flex;
 justify-content: flex-start;
 flex-wrap: wrap;
@@ -13,33 +17,96 @@ flex-wrap: wrap;
   width: 25%;
   min-width: 25%;
   max-width: 25%;
-  max-height: 400px;
+  max-height: 444px;
   padding: 10px 10px;
-  overflow: hidden;
-  img {
+  /* overflow: hidden; */
+  .lazyload-wrapper {
+    width:100%;
     height: 90%;
+  }
+  img {
+    height: 100%;
     width: 100%;
     object-fit: cover;
   }
   footer {
+    margin-top: 5px;
     width: 100%;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
+    .ig_hanle {
+      font-weight: 700;
+    }
   }
 }
+@media(max-width:992px) {
+    .item {
+      box-sizing: border-box;
+      flex: 1;
+      width: 33.333%;
+      min-width: 33.333%;
+      max-width: 33.333%;
+      max-height: 444px;
+      padding: 10px 10px;
+    }
+}
+@media(max-width:768px) {
+    .item {
+      box-sizing: border-box;
+      flex: 1;
+      width: 50%;
+      min-width: 50%;
+      max-width: 50%;
+      max-height: 444px;
+      padding: 10px 10px;
+    }
+}
+@media(max-width:576px) {
+    .item {
+      box-sizing: border-box;
+      flex: 1;
+      width: 100%;
+      min-width: 100%;
+      max-width: 100%;
+      max-height: 400px;
+      padding: 10px 10px;
+    }
+}
 `
+const LazyImage = (props) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+  useEffect(() => {
+    const img = new Image()
+    img.src = props.src
+    console.log(img.width)
+    img.onload = () => {
+      console.log('加载完成')
+      setIsLoaded(true)
+    }
+  }, [props.src])
+
+  return (
+    <>
+    {
+      isLoaded ? <img src={props.src} alt=''/> : <Loading />
+    }
+    </>
+  )
+}
 
 const PostItem = ({post}) =>  {
   const month = day(post.date).month()
   const year = day(post.date).year()
   return  (
     <div className='item'>
-      <img src={post.image}/>
+      <LazyLoad height={450}>
+      <LazyImage src={post.image}/>
+      </LazyLoad>
       <footer>
         <div className='ig_hanle'>{post.ig_handle}</div>
-        <div className='date'>{month} {year}</div>
+        <div className='date'>{monthObj[month]} {year}</div>
       </footer>
     </div>
    )
