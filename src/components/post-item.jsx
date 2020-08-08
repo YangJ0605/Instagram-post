2902fa75e52aa922b8bdf8d33f5788c653948c0a
YangJ0605 <1442122744@qq.com>
2020-08-08 11:28:05 +0800

    img layout

--- src/components/post-item.jsx
+++ src/components/post-item.jsx
@@ -7,8 +7,19 @@ display: flex;
 justify-content: flex-start;
 flex-wrap: wrap;
 .item {
-  margin-right: 10px;
-  width: 24%;
+  box-sizing: border-box;
+  flex: 1;
+  width: 25%;
+  min-width: 25%;
+  max-width: 25%;
+  height: 400px;
+  padding: 10px 10px;
+  overflow: hidden;
+  img {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
 }
 `
 
